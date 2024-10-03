039408b6f53d9255656bd51bed500759bdc7f8d4
joao <vitordire@gmail.com>
2024-10-03 10:32:57 -0300

    first commit

---
+++ coocking-up/src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import MainContent from './components/MainContent.vue';
+
+export default {
+  components: { Banner, MainContent }
+}
+</script>
+
+<template>
+  <Banner />
+  <MainContent />
+</template>
+
+<style scoped>
+
+</style>
